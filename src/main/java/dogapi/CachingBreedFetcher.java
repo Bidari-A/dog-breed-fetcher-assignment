6fd1aa45fe4607c02f160e686e8341530a01bcb2
avabidari <ava.bidari@mail.utoronto.ca>
2025-10-17 23:13:57 -0400

    all done, but still have to remove TODOs

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -13,7 +13,7 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
+    // Task 2: Complete this class
     private final BreedFetcher higherfetcher;
     private final Map<String, List<String>> cache = new HashMap<>();
     private int callsMade = 0;
--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -24,13 +24,13 @@ public class Main {
      * returned by the fetcher
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher)
-            throws BreedFetcher.BreedNotFoundException {
-        // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
+    {
+        // Task 3 implement this code so that it is entirely consistent with its provided documentation.
         // return statement included so that the starter code can compile and run.
         try {
             return breedFetcher.getSubBreeds(breed).size();
         }  catch (BreedFetcher.BreedNotFoundException e) {
-            return 0;
+            return 0; //maybe this will work
         }
     }
 }
